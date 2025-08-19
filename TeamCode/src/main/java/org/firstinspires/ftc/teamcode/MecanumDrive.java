4d82f23748ea0b94cf201f9bc27fb91716beaeef
ClassicalEngineers8579 <classy.engineers.ftc@gmail.com>
2025-08-18 20:03:00 -0400

    Updated gains for Road Runner
    Re-finished roadrunner tuning. For real this time.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -82,12 +82,12 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
+        public double axialGain = 3;
         public double lateralGain = 0.0;
-        public double headingGain = 0.0; // shared with turn
+        public double headingGain = 1; // shared with turn
 
         public double axialVelGain = 0.0;
-        public double lateralVelGain = 0.0;
+        public double lateralVelGain = 2;
         public double headingVelGain = 0.0; // shared with turn
     }
 
