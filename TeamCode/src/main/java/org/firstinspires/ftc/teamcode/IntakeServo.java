5b5455619879650d2ab93d9235949b515ea825f4
ClassicalEngineers8579 <classy.engineers.ftc@gmail.com>
2025-08-28 18:04:41 -0400

    Update IntakeServo.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeServo.java
@@ -1,24 +1,35 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "IntakeServoTest", group = "Robot")
 @Config
 public class IntakeServo extends OpMode {
+    //BUCKET VARIABLES
     public static double BUCKET_UNLOAD=0.80;
     public static double BUCKET_LOAD=0.34;
     public static double BUCKET_MID=0.6;
 
+    //VERTICAL SLIDE VARIABLES
+    public static int verticalUp=3500;
+    public static boolean verticalDown;
+
     CRServo Intake;
     Servo Bucket;
+    DcMotor Vertical;
     @Override
     public void init() {
-    Intake = hardwareMap.get(CRServo.class, "IntakeServo");
-    Bucket = hardwareMap.get(Servo.class, "Bucket Servo");
+        Intake = hardwareMap.get(CRServo.class, "IntakeServo");
+        Bucket = hardwareMap.get(Servo.class, "Bucket Servo");
+        Vertical = hardwareMap.get(DcMotor.class, "Vertical Slide");
+        telemetry=new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
     }
 
@@ -52,11 +63,28 @@ public class IntakeServo extends OpMode {
         else{
             Intake.setPower(0);
         }
+        if (gamepad2.triangle) {
+            if(verticalDown == false){
+                Vertical.setPower(1);
+                Vertical.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                Vertical.setTargetPosition(verticalUp);
+            }
+        }
+        else if (gamepad2.cross) {
+            Vertical.setPower(-0.35);
+            if(verticalDown == true){
+                Vertical.setPower(0);
+            }
+        }
 
         //telementry stuff
+        //Vertical Slide
+        telemetry.addData("Vert. Slide POS", Vertical.getCurrentPosition());
+        telemetry.addData("Vert. Slide PWR", Vertical.getPower());
+
         //Intake
-        telemetry.addData("Intake Power", Intake.getPower());
-        telemetry.addData("Intake Direction???", Intake.getDirection());
+        telemetry.addData("Intake PWR", Intake.getPower());
+        telemetry.addData("Intake Direction", Intake.getDirection());
         //bucket
         telemetry.addData("Bucket Position", Bucket.getPosition());
     }
